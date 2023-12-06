3ab1868cc2c4c7f4493012556e562a0773d68d4f
Kondeti Sivaji <shivajikondeti555@gmail.com>
2023-12-07 00:59:40 +0530

    style: add dark mode

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
+    <div className="flex justify-center items-center">
       <h2 className="font-bold capitalize text-2xl text-gray-800">
         Dashboard Page..
       </h2>
--- components/header.tsx
+++ components/header.tsx
@@ -1,38 +1,39 @@
-import Link from "next/link";
-import { FC } from "react";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/nextjs";
-import { ThemeToggle } from "@/components/theme-toggle";
+import Link from 'next/link';
+import { FC } from 'react';
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import { ThemeToggle } from '@/components/theme-toggle';
 
 interface headerProps {}
 
 const Header: FC<headerProps> = ({}) => {
-  return (
-    <header className="w-screen h-16 bg-slate-50 sticky top-0 left-0">
-      <nav className="w-[85vw] h-full mx-auto flex justify-between items-center">
-        <Link href={"/"}>
-          <div>shivaji.dev</div>
-        </Link>
-        <div className="flex space-x-6 items-center">
-          <ThemeToggle />
-          <div>
-            <SignedIn>
-              {/* Mount the UserButton component */}
-              <UserButton />
-            </SignedIn>
-            <SignedOut>
-              {/* Signed out users get sign in button */}
-              <SignInButton />
-            </SignedOut>
-          </div>
-        </div>
-      </nav>
-    </header>
-  );
+   return (
+      <header className='sticky left-0 top-0 h-16 w-full shadow dark:shadow-white'>
+         <nav className='mx-auto flex h-full w-[80vw] items-center justify-between '>
+            <Link href={'/'}>
+               <div>
+                  <h2 className='text-lg font-bold text-zinc-600 dark:text-white lg:text-xl'>
+                     Shivaji.dev
+                  </h2>
+               </div>
+            </Link>
+            <div className='flex items-center space-x-4 lg:space-x-6'>
+               <ThemeToggle />
+               <div>
+                  <SignedIn>
+                     {/* Mount the UserButton component */}
+                     <UserButton afterSignOutUrl='/' />
+                  </SignedIn>
+                  <SignedOut>
+                     {/* Signed out users get sign in button */}
+                     <Link href={'/sign-in'}>
+                        <div className='dark:text-white'>SignIn</div>
+                     </Link>
+                  </SignedOut>
+               </div>
+            </div>
+         </nav>
+      </header>
+   );
 };
 
 export default Header;
